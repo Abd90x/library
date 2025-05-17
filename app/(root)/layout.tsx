4fa33bc9d542991b70afa0f6974952153b7cc177
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2025-05-18 01:24:31 +0300

    css edit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main className="root-container">
+      {/* Header */}
+
+      <div className="mx-auto max-w-7xl">
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-7xl test">Home</h1>
+    </>
+  );
+};
+export default Home;
