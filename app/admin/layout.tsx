6cfb7a7f85f19cb4e468780b85c54f0cd051c96f
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2025-05-21 22:23:07 +0300

    add admin role validation to admin layout

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -5,11 +5,23 @@ import "@/styles/admin.css";
 
 import Sidebar from "@/components/admin/sidebar";
 import Header from "@/components/admin/header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
 
   if (!session) return redirect("/sign-in");
+
+  const isAdmin = await db
+    .select({ isAdmin: users.role })
+    .from(users)
+    .where(eq(users.id, session.user?.id!))
+    .limit(1)
+    .then((res) => res[0]?.isAdmin === "ADMIN");
+
+  if (!isAdmin) return redirect("/");
 
   return (
     <main className="flex min-h-screen w-full flex-row">
