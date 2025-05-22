1b7ea7d23821e5412f36e68de3964f8be532c10e
Abdellatif Alrefahi <abdellatif.alrefahi@gmail.com>
2025-05-22 15:03:59 +0300

    adjust color picker UI and add text stroke styling
    Refactor the color picker component to improve visual consistency by removing redundant styles, adding text stroke effects, and adjusting button styling. Also, introduce a new `.text-stroke` utility class in global CSS for reusable text stroke effects.

--- components/admin/color-picker.tsx
+++ components/admin/color-picker.tsx
@@ -23,15 +23,16 @@ const ColorPicker = ({ value, onPickerChange }: Props) => {
           "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-white px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
           "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
           "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-          "h-14 relative"
+          "h-14 relative "
         )}
+        style={{ backgroundColor: value }}
       >
-        <div className="flex flex-row items-center">
+        <div className="flex flex-row items-center font-semibold uppercase text-stroke text-white">
           <p>#</p>
           <HexColorInput
             color={value}
             onChange={onPickerChange}
-            className="hex-input"
+            className="hex-input text-shadow-2xl text-stroke uppercase"
           />
         </div>
         <Popover>
@@ -39,7 +40,7 @@ const ColorPicker = ({ value, onPickerChange }: Props) => {
             <Button
               type="button"
               size="icon"
-              className="bg-admin hover:bg-admin/80 text-white absolute end-2.5 top-2.5"
+              className="border bg-white hover:bg-border text-admin absolute end-2.5 top-2.5 "
             >
               <Pipette />
             </Button>
